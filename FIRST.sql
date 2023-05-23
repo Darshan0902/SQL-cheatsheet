877f4a09f866756bd5816b4fd752f14438c5787b
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-05-23 14:46:09 +0530

    Update FIRST.sql

--- FIRST.sql
+++ FIRST.sql
@@ -1,4 +1,16 @@
 
-```
+
 https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all
-```
+
+Use the above link and enter all the commands I have entered below to perform basic SQL operations : 
+
+
+1.)  Displaying whole table : 
+
+SELECT * FROM tablename
+
+2.) To display a few columns  :
+
+select column_1 , column_2 from tablenmae 
+
+3.) 
